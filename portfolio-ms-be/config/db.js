bbc9d842a25ed5d4fc31561a9d706df22a42220d
moiz-xyz <a.moizniaxi123@gmail.com>
2026-04-08 21:46:23 +0500

    added

--- portfolio-ms-be/config/db.js
+++ portfolio-ms-be/config/db.js
@@ -1,12 +1,15 @@
 import pkg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
 const { Pool } = pkg;
-import "dotenv/config";
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 export default pool;
